143f407e47be4a535fc363a018dc8ed7b52fbbec
jiftechnify <jiftech.stlfy@gmail.com>
2021-08-13 01:36:52 +0000

    Fix: split webpack config, generate sourcemap only in develop mode

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  mode: "production",
   entry: "./src/index.js",
-  devServer: {
-    contentBase: "./public",
-    port: 8080,
-    host: "0.0.0.0",
-  },
   output: {
     filename: "script.js",
     path: path.resolve(__dirname, "public"),
